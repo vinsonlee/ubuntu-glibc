deabce9135891f6caf136743cc373dc748ea5b21
Aurelien Jarno <aurel32@debian.org>
2016-07-11 22:45:50 +0200

    Import patches-unapplied version 2.23.90+20160711.c10f90d-1 to debian/experimental
    Imported using usd-importer.
    
    Publish parent: 4b58fa60af3059c8d4d41706c3ce43bb5eb42115
    
    New changelog entries:
      [ Samuel Thibault ]
      * patches/hurd-i386/submitted-malloc-setstate.diff: New patch to fix
        malloc_setstate.
      * testsuite-xfail-debian.mk: Update hurd-i386 testsuite results, no actual
        regression.
      [ Aurelien Jarno ]
      * New upstream snapshot from 20160711:
        - debian/patches/localedata/locale-de_LI.diff: upstreamed.
        - debian/patches/hppa/submitted-libm-test-ulps.diff: upstreamed.
      * debian/testsuite-xfail-debian.mk: allow conform/XPG3/sys/stat.h
        and conform/XPG4/sys/stat.h to fail on mips and mipsel, similarly
        to the others sys/stat.h conform tests. Group all theses entries
        and an explanation.
      * debian/testsuite-xfail-debian.mk: do not allow math/test-fenv to
        fail now that we do not have Loongson 2 buildds anymore.
      * debian/patches/kfreebsd/local-sysdeps.diff, local-fbtl.diff:
        update to revision 6087 (from glibc-bsd).
      * debian/testsuite-xfail-debian.mk: allow more failures on kfreebsd-*,
        mostly due to the fsid_t type, and which are not regressions.
      * testsuite-xfail-debian.mk: Update x32 testsuite results, no actual
        regression.
      * Replace debian/patches/alpha/submitted-trunc.diff by
        debian/patches/alpha/submitted-math-fixes.diff containing additional
        math fixes.
      * debian/rules: better glibc version detection for snapshots.

--- sysdeps/unix/sysv/linux/aarch64/bits/hwcap.h
+++ sysdeps/unix/sysv/linux/aarch64/bits/hwcap.h
@@ -1,6 +1,6 @@
-/* Copyright (C) 1997-2016 Free Software Foundation, Inc.
+/* Defines for bits in AT_HWCAP.  AArch64 Linux version.
+   Copyright (C) 2016 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
-   Contributed by Ulrich Drepper <drepper@cygnus.com>, 1997.
 
    The GNU C Library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
@@ -16,22 +16,19 @@
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
 
-#include <unistd.h>
-#include <sysdep-cancel.h>
-
-#ifndef __OFF_T_MATCHES_OFF64_T
-
-# ifndef __NR_pread
-#  define __NR_pread __NR_pread64
-# endif
-
-ssize_t
-__libc_pread (int fd, void *buf, size_t count, off_t offset)
-{
-  return SYSCALL_CANCEL (pread, fd, buf, count,
-			 __ALIGNMENT_ARG SYSCALL_LL (offset));
-}
-
-strong_alias (__libc_pread, __pread)
-weak_alias (__libc_pread, pread)
+#if !defined (_SYS_AUXV_H)
+# error "Never include <bits/hwcap.h> directly; use <sys/auxv.h> instead."
 #endif
+
+/* The following must match the kernel's <asm/hwcap.h>.  */
+#define HWCAP_FP		(1 << 0)
+#define HWCAP_ASIMD		(1 << 1)
+#define HWCAP_EVTSTRM		(1 << 2)
+#define HWCAP_AES		(1 << 3)
+#define HWCAP_PMULL		(1 << 4)
+#define HWCAP_SHA1		(1 << 5)
+#define HWCAP_SHA2		(1 << 6)
+#define HWCAP_CRC32		(1 << 7)
+#define HWCAP_ATOMICS		(1 << 8)
+#define HWCAP_FPHP		(1 << 9)
+#define HWCAP_ASIMDHP		(1 << 10)
